fd003136de725bf676bb7b87ee0e23f596e6df2d
Asbj14777 <andersenasbjornmeyer@gmail.com>
2026-02-03 11:03:33 +0100

    Add car rental journal functionality
    Implemented a basic car rental journal with functionality to add and display journal entries.

--- car-rental/Program.cs
+++ car-rental/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using car_rental;
+namespace car_rental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Rental Journal");
+            Journal journal = new Journal();
+
+            journal.Add(new Journal(2, "audi", "az29752"));
+
+            var items = journal.GetJournal();
+
+            foreach (var item in items)
+                Console.WriteLine(item.ToString());
+        }
+    }
+}
